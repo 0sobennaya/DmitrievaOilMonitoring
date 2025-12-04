ab553153dd6fa1a5c70e8a9ca779f2837297e3c0
0sobennaya <143941641+0sobennaya@users.noreply.github.com>
2025-12-04 22:08:05 +0300

    Try to do pump-card

---
+++ DmitrievaClient/src/app/app/pump-page/pump-card/pump-card.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCard } from '@angular/material/card';
+
+@Component({
+  selector: 'app-pump-card',
+  imports: [MatCard],
+  templateUrl: './pump-card.html',
+  styleUrl: './pump-card.css',
+})
+export class PumpCard {
+
+}
--- DmitrievaClient/src/app/app/pump-page/pump-page.ts
+++ DmitrievaClient/src/app/app/pump-page/pump-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PumpCard } from "./pump-card/pump-card";
 
 @Component({
   selector: 'app-pump-page',
-  imports: [],
+  imports: [PumpCard],
   templateUrl: './pump-page.html',
   styleUrl: './pump-page.css',
 })
